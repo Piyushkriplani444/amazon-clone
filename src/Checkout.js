a2f8403579c1afabed03366e26ce71f24e7593b1
Piyush Kriplani <piyushkriplani444@gmail.com>
2021-07-20 15:47:11 +0530

    add check point

--- src/Checkout.js
+++ src/Checkout.js
@@ -1,13 +1,21 @@
 import React from 'react';
 import './Checkout.css';
+import Subtotal from './Subtotal';
 function Checkout() {
     return (
         <div className="checkout">
             <div className="checkout__left">
-                <img className="checkout__ad"  src=" "/>
-            </div>
-
+                <img className="checkout__ad"  src="https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcTv1p6HQcwl799LrBvYfhGd3pDqBpnyWV5xUQ&usqp=CAU"
+                 alt=""/>
             
+            <div>
+                <h2 className="checkout__title">Your Shooping Basket</h2>
+            </div>
+            </div>
+            <div className="checkout__right">
+                <Subtotal/>
+              
+            </div>
         </div>
     );
 }
