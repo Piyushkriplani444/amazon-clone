6ad85a40572cec8c3c59aaa530b34367eb93ddf3
Piyush Kriplani <piyushkriplani444@gmail.com>
2021-07-21 15:08:46 +0530

    added login page

--- src/App.js
+++ src/App.js
@@ -3,22 +3,29 @@ import { BrowserRouter as Router, Route, Switch } from "react-router-dom";
 import Header from './Header.js';
 import Home from "./Home";
 import Checkout from './Checkout';
+import Login from './Login';
 
 function App() {
   return (
     //BEM
     <Router>
     <div className="App">
-    <Header/>
+    
       
       
        <Switch>
-       
+       <Route path="/login">
+         <Login/>
+
+    
+         </Route>
          <Route path="/checkout">
+         <Header/>
            <Checkout/>
          </Route>
 
          <Route path="/">
+         <Header/>
         <Home/>
          </Route>
          
